6646f0fb9ea97eed745fbbeab9a4e877e3f43667
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-10-30 00:08:34 +0200

    init

---
+++ EggscapeRoute/App/EggscapeRouteApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggscapeRouteApp.swift
+//  EggscapeRoute
+//
+//  Created by Serhii Babchuk on 29.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct EggscapeRouteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ERLaunchView()
+        }
+    }
+}
---
+++ Pods/Target Support Files/Pods-EggscapeRoute/Pods-EggscapeRoute-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EggscapeRoute : NSObject
+@end
+@implementation PodsDummy_Pods_EggscapeRoute
+@end
